4e423c6867681eb7ebe14c4fe5c5bd8b5c119a76
satya4sightai <165569956+satya4sightai@users.noreply.github.com>
2024-06-24 19:23:19 +0530

    Add files via upload

---
+++ js/wow.js
@@ -0,0 +1,9 @@
+wow = new WOW({
+    animateClass: 'animated',
+    offset: 100,
+    callback: function(box) {
+        // console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+    }
+});
+
+wow.init();
